8cc194aab289c6c274ff44881ee39135acefc364
abhay0480-eng <abhay0480sharma@gmail.com>
2022-04-22 08:21:55 +0530

    design blackjack game with custom functionalities

---
+++ blackjack.js
@@ -0,0 +1,120 @@
+
+
+
+let cards_array = []
+
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+
+let title = document.getElementById("title")
+let result = document.getElementById("result")
+let cards = document.getElementById("cards")
+let meme = document.getElementById("meme")
+
+let message = ""
+let count = 0
+
+
+
+
+
+
+let player = {
+    Name: "You won )) ",
+    chips: 120
+}
+let player_info = document.getElementById("player-info")
+
+
+
+
+function getRandomCard() {
+    let random_no = Math.floor(Math.random() * 13 + 1)
+    if (random_no === 1) {
+        return 11
+    }
+    else if (random_no > 10) {
+        return 10
+    }
+    else
+        return random_no
+
+}
+
+
+
+function startGame() {
+    meme.style.display = "none"
+    isAlive = true
+    player_info.textContent = ''
+    hasBlackJack = false
+    cards_array = [];
+    if (isAlive && !hasBlackJack) {
+
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards_array = [firstCard, secondCard]
+        sum = firstCard + secondCard
+
+        renderGame()
+
+
+    }
+
+
+
+}
+
+
+
+
+
+function renderGame() {
+    cards.textContent = '';
+    for (let i = 0; i < cards_array.length; i++) {
+        cards.textContent += cards_array[i] + " "
+
+    }
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? "
+
+
+    }
+    else if (sum === 21) {
+        message = "Wohooo! you won blackjack!"
+        hasBlackJack = true
+
+    }
+    else {
+        message = "Sorry! you are out of game "
+        isAlive = false
+    }
+
+    title.textContent = message
+    result.textContent = "Sum: " + " " + sum
+
+
+}
+
+function newCard() {
+
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomCard()
+        sum += card
+        cards_array.push(card)
+
+        renderGame()
+
+    }
+    if (hasBlackJack) {
+        player_info.textContent = player.Name + " : " + "  $" + player.chips
+        meme.style.display = "block"
+    }
+
+
+
+
+}
+
